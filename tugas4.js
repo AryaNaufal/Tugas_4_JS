3a5621f86033c80a9b06e2739bc8ada5b8ed17ad
Arya Naufal <arya.naufal.andt@gmail.com>
2022-01-01 18:03:27 +0700

    TUGAS 4 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas4.js
@@ -0,0 +1,14 @@
+let andi = 170;
+let tono = 160;
+let budi = 150;
+
+if(andi > tono){
+    console.log("andi siswa tertinggi ke 1")
+    if(tono > budi){
+        console.log("tono siswa tertinggi ke 2")
+    } if(budi < andi){
+        console.log("budi siswa tertinggi ke 3")
+    }
+} else {
+    console.log("andi siswa terpendek")
+}
